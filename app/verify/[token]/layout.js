3d934105f16d0f262fc8007644ee4c6f0ca12623
Subhan <subhanramzan215@gmail.com>
2026-06-27 23:48:19 +0500

    New-Change

--- app/verify/[token]/layout.js
+++
@@ -1,8 +0,0 @@
-export const metadata = {
-  title: "Verify Your Account - Subhan Portfolio",
-  description: "Verify your account to access your profile and visit projects.",
-};
-
-export default function VerifyLayout({ children }) {
-  return <div>{children}</div>;
-}
